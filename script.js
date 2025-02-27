d43661dfdce43fcd9e87219e65e04d21ea57ce00
Tom Khrustalev <53542370+tomkhr@users.noreply.github.com>
2025-02-26 21:14:13 -0700

    Created working code for the first JS filter and added more HTML and CSS

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("Test")
+
+const categoryFilter = document.getElementById('category');
+const countryFilter = document.getElementById('country');
+const decadeFilter = document.getElementById('decade');
+const articles = document.querySelectorAll('#pieces article');
+
+categoryFilter.addEventListener('change', () => {
+    const selectedCategory = categoryFilter.value;
+
+    for (let i = 0; i < articles.length; i++) {
+        const article = articles[i];
+        const articleCategory = article.getAttribute('data-category');
+
+        if (selectedCategory === 'all' || selectedCategory === articleCategory) {
+            article.style.display = 'block';  
+        } else {
+            article.style.display = 'none';
+        }
+    }
+});
+
+
+countryFilter.addEventListener('change', () => {});
+
+
+decadeFilter.addEventListener('change', () => {});
